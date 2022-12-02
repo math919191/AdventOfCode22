0b747214084a896a7f0a8a50e32e951cfd469df2
math919191 <math919191@gmail.com>
2022-12-01 18:21:40 -0700

    got input / part 1!

---
+++ day1.py
@@ -0,0 +1,13 @@
+from sys import stdin, stdout
+
+counter = 0
+totals = []
+for line in stdin:
+    if (line != "\n"):
+        counter += int(line)
+    else:
+        totals.append(counter)
+        counter = 0
+totals.sort(reverse=True)
+print("Part 1: ", totals[0])
+print("Part 2: ", totals[0] + totals[1] + totals[2])
---
+++ hello.py
@@ -0,0 +1,5 @@
+from sys import stdin, stdout
+
+# for line in stdin:
+#     print(line)
+print("hello")
