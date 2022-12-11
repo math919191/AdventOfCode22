e54c6bc3f82576b47b1f12e763ddc3fbdc000fce
math919191 <math919191@gmail.com>
2022-12-10 18:28:14 -0700

    printing tree / fixed bug

--- day7attempt2.py
+++ day7attempt2.py
@@ -12,7 +12,7 @@ class Tree(object):
 
     def __init__(self, name='root', parent='parent', size=0, children=None):
         self.name = name
-        self.size = 0
+        self.size = size
         self.children = []
         self.parent = parent
         if children is not None:
@@ -25,6 +25,9 @@ class Tree(object):
     def get_parent(self):
         return self.parent
 
+    def get_children(self):
+        return self.children
+
     def add_child(self, node):
         #assert isinstance(node, Tree)
         self.children.append(node)
@@ -35,12 +38,10 @@ class Tree(object):
     def edit_children(self, dirToChange, newChildren):
 
         for i in range(len(self.children)):
-            print(str(self.children[i].__repr__()), str(dirToChange))
             if (str(self.children[i].__repr__()) == str(dirToChange)):
-                # for child in newChildren:
-                print(self.children[i].__repr__())
+                # print(self.children[i].get_children())
                 self.children[i].set_children(newChildren)
-
+                # print(self.children[i].get_children())
                 return self.children[i]
 
     def get_child(self, dirName):
@@ -56,55 +57,61 @@ root.add_child(slashTree)
 currLine = 0
 
 
-def callThis():
-    print("hello")
-
-
-global DEATH
-DEATH = False
-
-
 def addDirAndChildren(dirName, currLineNum, parentTree, input):
+    print(parentTree)
+    print("parent: ", parentTree.__repr__())
+    print("child: ", dirName)
     # the line that we are currently on with dir name
-    dirNameTree = parentTree.get_child(dirName)
     if (currLineNum == (len(input))):
         print("DONE")
         return
 
-    myLine = input[currLine].split(" ")
+    #myLine = input[currLine].split(" ")
     # if (myLine[1] == "cd" and myLine[2] != ".."):
     if (dirName != ".."):
+        dirNameTree = parentTree.get_child(dirName)
+
         currDirChildren = []  # init
 
         currLineNum += 2  # jump over the ls
 
-        #sizeOrDir, name = input[currLineNum].split(" ")
         # keep going till we hit a cd command
         while (currLineNum < (len(input)) and input[currLineNum].split(" ")[0] != "$"):
             sizeOrDir, name = input[currLineNum].split(" ")
-            currDirChildren.append(
-                Tree(name, dirNameTree, sizeOrDir, None))
-            print(input[currLineNum].split(" "))
+            currDirChildren.append(Tree(name, dirNameTree, sizeOrDir, None))
             currLineNum += 1
 
-        print("parentTreeName", parentTree.__repr__())
-        childTree = parentTree.edit_children(
-            dirName, currDirChildren)
-        print("ChildTreeName", childTree.__repr__())
+        #print("parentTreeName", parentTree.__repr__())
+        childTree = parentTree.edit_children(dirName, currDirChildren)
+        #print("ChildTreeName", childTree.__repr__())
 
 #        if (input[currLineNum].split(" ")[2] != ".."):
         if (int(currLineNum) == int(len(input))):
             print("DONE")
             return
 
-        print(currLineNum, len(input))
         addDirAndChildren(input[currLineNum].split(
             " ")[2], currLineNum, childTree, input)
     elif (dirName == ".."):
-        print("moving backwards")
-        print(parentTree)
+        print("going back")
         addDirAndChildren(input[currLineNum+1].split(" ")
                           [2], currLineNum+1, parentTree.get_parent(), input)
 
 
 addDirAndChildren("/", 0, root, lines)
+
+
+def printTree(tree, indent):
+    for child in tree.get_children():
+        # print(child.size)
+        if child.size == "dir":
+            #    print(child.get_children())
+            print(indent, "-", child.__repr__())
+
+            printTree(child, indent + "  ")
+        else:
+            print(indent, "-", child.__repr__())
+
+
+print("calling tree")
+print(printTree(slashTree, " "))
