3e6e2d9805fc43755b7a60689df76cb74c0b45e3
math919191 <math919191@gmail.com>
2022-12-20 21:49:02 -0700

    cleaned up day 18

--- day18.py
+++ day18.py
@@ -13,7 +13,6 @@ for x in range(size):
             xMap[y].append(False)
     map.append(xMap)
 
-# 5,7,4
 for line in input:
     coor = line.split(",")
     coor = [int(x) for x in coor]
@@ -37,113 +36,8 @@ def checkNumOfFreeSides(map, coor):
     return total
 
 
-def checkNumOfOutsideFreeSides(map, coor):
-    total = 0
-    if (map[coor[0]-1][coor[1]][coor[2]] == outside):
-        total += 1
-    if (map[coor[0]+1][coor[1]][coor[2]] == outside):
-        total += 1
-    if (map[coor[0]][coor[1]-1][coor[2]] == outside):
-        total += 1
-    if (map[coor[0]][coor[1]+1][coor[2]] == outside):
-        total += 1
-    if (map[coor[0]][coor[1]][coor[2]+1] == outside):
-        total += 1
-    if (map[coor[0]][coor[1]][coor[2]-1] == outside):
-        total += 1
-    return total
-
-
 def checkBounds(num):
     if (num >= 0 and num < (size)):
-        return True
-    else:
-        return False
-
-
-def greedilyFillOutside(map, x, y, z):
-    # check bounds
-    if (checkBounds(x) and checkBounds(y) and checkBounds(z)):
-        pass
-    else:
-        return map
-
-    # check if already touching outsides
-    if (map[x][y][z] == "outside"):
-        if (map[x+1][y][z] == False):
-            map[x+1][y][z] = "outside"
-            greedilyFillOutside(map, x+1, y, z)
-        if (map[x-1][y][z] == False):
-            map[x-1][y][z] = "outside"
-            greedilyFillOutside(map, x-1, y, z)
-        if (map[x][y+1][z] == False):
-            map[x][y+1][z] = "outside"
-            greedilyFillOutside(map, x, y+1, z)
-        if (map[x][y-1][z] == False):
-            map[x][y-1][z] = "outside"
-            greedilyFillOutside(map, x, y-1, z)
-        if (map[x][y][z+1] == False):
-            map[x][y][z+1] = "outside"
-            greedilyFillOutside(map, x, y, z+1)
-        if (map[x][y][z-1] == False):
-            map[x][y][z-1] = "outside"
-            greedilyFillOutside(map, x, y, z-1)
-
-    else:
-        return map
-
-
-def checkIfOutSide(coor):
-    if (map[coor[0]][coor[1]][coor[2]] == True):
-        return False
-    # else:
-    #     return True
-
-    if (map[coor[0]-1][coor[1]][coor[2]] == outside):
-        return True
-    elif (map[coor[0]+1][coor[1]][coor[2]] == outside):
-        return True
-    elif (map[coor[0]][coor[1]-1][coor[2]] == outside):
-        return True
-    elif (map[coor[0]][coor[1]+1][coor[2]] == outside):
-        return True
-    elif (map[coor[0]][coor[1]][coor[2]+1] == outside):
-        return True
-    elif (map[coor[0]][coor[1]][coor[2]-1] == outside):
-        return True
-    else:
-        return False
-
-
-def fillOutside(map):
-    for i in range(size):
-        for j in range(size):
-            map[i][j][size-1] = outside
-            map[i][j][0] = outside
-            map[i][size-1][j] = outside
-            map[i][0][j] = outside
-            map[size-1][i][j] = outside
-            map[0][i][j] = outside
-
-    changes = True
-    while (changes):
-        changes = False
-        for x in range(1, size-1):
-            for y in range(1, size-1):
-                for z in range(1, size-1):
-                    if (map[x][y][z] == False):
-                        if (checkIfOutSide([x, y, z])):
-                            map[x][y][z] = outside
-                            changes = True
-
-
-def seeIfAllAroundTrue(x, y, z):
-    if (map[x+1][y][z]
-        and map[x-1][y][z]
-        and map[x][y+1][z]
-        and map[x][y-1][z]
-        and map[x][y][z+1]
-            and map[x][y][z-1]):
         return True
     else:
         return False
@@ -188,7 +82,6 @@ def flood_fill(x, y, z):
         if (check_validity(x1, y1, z1-1, seen)):
             q.append((x1, y1, z1-1))
             seen.add((x1, y1, z1-1))
-    print("finished")
 
 
 def checkNumOfInsideSurface(map, coor):
@@ -208,34 +101,24 @@ def checkNumOfInsideSurface(map, coor):
     return total
 
 
-# map[0][0][0] = "outside"
-# greedilyFillOutside(map, 0, 0, 0)
-# fillOutside(map)
-flood_fill(0, 0, 0)
-print("flood filled")
+answer1 = 0
+for x in range(size-1):
+    for y in range(size-1):
+        for z in range(size-1):
+            if (map[x][y][z] == True):
+                answer1 += checkNumOfFreeSides(map, [x, y, z])
+print("Part 1:", answer1)
 
-answer = 0
-answer2 = 0
+flood_fill(0, 0, 0)
+
+insideSurface = 0
 for x in range(size-1):
     for y in range(size-1):
         for z in range(size-1):
             if (map[x][y][z] == False):
-                answer += checkNumOfInsideSurface(map, [x, y, z])
-            # if (map[x][y][z] == True):
-            #     answer += checkNumOfOutsideFreeSides(map, [x, y, z])
-            #     answer2 += checkNumOfFreeSides(map, [x, y, z])
-            #     print(answer)
-            #     print(answer2)
-                #
-                #  print(x, y, z)
-                # print(answer)
-            #
-            # else:
-            #     if (seeIfAllAroundTrue(x, y, z) == True):
-            #         answer -= 6
-print(answer)
+                insideSurface += checkNumOfInsideSurface(map, [x, y, z])
 
-print(4482 - answer)
+print("Part 2:", answer1 - insideSurface)
 # 4454 is too low
 
 # part 2 2445 ? nope ... too low
@@ -244,26 +127,3 @@ print(4482 - answer)
 # part 2 4194 is just wrong
 
 # part 1 answer was 4482
-#######
-# added = True
-# counter = 0
-# while (counter < size/2):
-#     #added = False
-
-#     upTo = size - counter - 1 - 1
-#     for i in range(counter, upTo):
-#         for j in range(counter, upTo):
-#             if (checkIfOutSide([i, j, upTo])):
-#                 map[i][j][upTo] = outside
-#             if (checkIfOutSide([i, j, counter])):
-#                 map[i][j][counter] = outside
-#             if (checkIfOutSide([i, upTo, j])):
-#                 map[i][upTo][j] = outside
-#             if (checkIfOutSide([i, counter, j])):
-#                 map[i][counter][j] = outside
-#             if (checkIfOutSide([upTo, i, j])):
-#                 map[upTo][i][j] = outside
-#             if (checkIfOutSide([counter, i, j])):
-#                 map[counter][i][j] = outside
-#     counter += 1
-#######
