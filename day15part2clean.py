fa2ebab73827ba770cb5da17aef34bec53cb3e6c
math919191 <math919191@gmail.com>
2022-12-26 19:19:21 -0700

    finished day 15 part 2

--- day15part2clean.py
+++ day15part2clean.py
@@ -10,6 +10,9 @@
 # contains beacon
 # contains function to check valid ranges
 # contains function to walk on edge
+global maxCoor
+maxCoor = 4000000
+
 
 class Sensor:
     def __init__(self, sx, sy, bx, by):
@@ -19,6 +22,7 @@ class Sensor:
         self.by = by
         self.diffX = abs(bx - sx)
         self.diffY = abs(by - sy)
+        self.diff = abs(bx - sx) + abs(by - sy)
 
         self.minYInRange = sy - self.diffY
         self.maxYInRange = sy + self.diffY
@@ -26,22 +30,35 @@ class Sensor:
         self.minXInRange = sx - self.diffX
         self.maxXInRange = sx + self.diffX
 
-    def checksIfInRange(self, givenX, givenY):
-        if (self.minYInRange < givenY < self.maxYInRange):
-            if (abs(self.sx - givenX) <= self.diffX):
-                return True
+    def checksIfOutOfRange(self, givenX, givenY):
+
+        distFromY = abs(givenY - self.sy)
+        distFromX = abs(givenX - self.sx)
+
+        if givenX < 0 or givenY < 0 or givenX > maxCoor or givenY > maxCoor:
+            return False
+
+        if self.diff < (distFromX + distFromY):
+            return True
         else:
             return False
-        return False
 
     def giveEdgePoints(self):
         edgePoints = []
-        for i in range(self.diffY, ):
+        for i in range(self.diff + 1):
             x = self.sx
             y = self.sy
 
-            for j in ([0, 1], [0, -1], [1, 0], [-1, 0]):
-                edgePoints.append([x + j[0], y + j[1]])
+            # for j in ([0, 1], [0, -1], [1, 0], [-1, 0]):
+
+            edgePoints.append(
+                [x - self.diff + 1 + i, y + i])
+            edgePoints.append(
+                [x + self.diff + 1 - i, y + i])
+            edgePoints.append(
+                [x - self.diff + 1 + i, y - i])
+            edgePoints.append(
+                [x + self.diff + 1 - i, y - i])
 
         return edgePoints
 
@@ -61,22 +78,38 @@ def readInput():
 
 
 sensors = readInput()
-for currSensor in sensors:
-    edges = currSensor.giveEdgePoints()
-    for edge in edges:
-        overlapping = False
-        for checkSensor in sensors:
-            if (checkSensor.checksIfInRange(edge[0], edge[1])):
-                overlapping = True
-                break
-            else:
-                continue
 
-        if overlapping == False:
-            print("answer: ", edge)
-            break
 
+def findAnswer():
+    for currSensor in sensors:
+
+        edges = currSensor.giveEdgePoints()
+        for edge in edges:
+            if (edge[0] == 14 and edge[1] == 11):
+                print("stop! wait a minute")
+            overlapping = False
+            overlappingSensors = 0
+
+            for checkSensor in sensors:
+
+                if (checkSensor.checksIfOutOfRange(edge[0], edge[1]) == False):
+                    overlapping = True
+                    overlappingSensors += 1
+                    break
+                else:
+                    continue
+
+            if overlapping == False:
+                print("answer: ", edge)
+                print("answer: ", edge[0]*4000000 + edge[1])
+                return
+
+            #print("overlapping sensors", overlappingSensors)
+            overlapping = False
+
+
+findAnswer()
 # walk on the outside of diamond
-    # for each position
-        # see if it overlaps with any other sensors
-        # if it doesn't overlap, then we have found our answer
+# for each position
+# see if it overlaps with any other sensors
+# if it doesn't overlap, then we have found our answer
